13512e141cf513d2e6d82e466c1e6853f2e1861e
Maxime Rossini <mrossini@maskott.com>
2017-03-08 14:02:32 +0100

    removed unit tests app settings config

--- xAPI.Client.Tests/Tests/BaseTest.cs
+++ xAPI.Client.Tests/Tests/BaseTest.cs
@@ -1,12 +1,21 @@
 ﻿using NUnit.Framework;
 using RichardSzalay.MockHttp;
+using System;
 using System.IO;
 using xAPI.Client.Configuration;
+using xAPI.Client.Resources;
 
 namespace xAPI.Client.Tests
 {
     public abstract class BaseTest
     {
+        private const string ENDPOINT_URI = "https://www.example.org/xAPI/";
+        private const string VERSION = "1.0.3";
+        private const string BASIC_HTTP_USERNAME = "test_username";
+        private const string BASIC_HTTP_PASSWORD = "test_password";
+        private const string OAUTH_CLIENT_ID = "test_client_id";
+        private const string OAUTH_CLIENT_SECRET = "test_client_secret";
+
         protected MockHttpMessageHandler _mockHttp;
         protected IXApiClient _client;
 
@@ -15,8 +24,8 @@ namespace xAPI.Client.Tests
         {
             this._mockHttp = new MockHttpMessageHandler();
             EndpointConfiguration config = this.GetEndpointConfiguration();
-            config.EndpointUri = Config.EndpointUri;
-            config.Version = Config.Version;
+            config.EndpointUri = new Uri(ENDPOINT_URI);
+            config.Version = XApiVersion.Parse(VERSION);
             config.HttpClient = this._mockHttp.ToHttpClient();
             this._client = XApiClientFactory.Create(config);
         }
@@ -32,8 +41,8 @@ namespace xAPI.Client.Tests
         {
             return new BasicEndpointConfiguration()
             {
-                Username = Config.BasicUsername,
-                Password = Config.BasicPassword
+                Username = BASIC_HTTP_USERNAME,
+                Password = BASIC_HTTP_PASSWORD
             };
         }
 
@@ -44,7 +53,7 @@ namespace xAPI.Client.Tests
 
         protected string GetApiUrl(string endpoint)
         {
-            return $"{Config.EndpointUri}{endpoint}";
+            return $"{ENDPOINT_URI}{endpoint}";
         }
     }
 }
