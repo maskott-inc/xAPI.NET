46f37c22713e5de4a71c8e5955bf9bb3ddc95bd7
Maxime Rossini <mrossini@maskott.com>
2017-03-13 18:19:05 +0100

    fix missing interface on statement ref

--- xAPI.Client/Resources/StatementRef.cs
+++ xAPI.Client/Resources/StatementRef.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace xAPI.Client.Resources
 {
-    public class StatementRef : ISubStatementTarget
+    public class StatementRef : IStatementTarget, ISubStatementTarget
     {
         [JsonProperty("id")]
         public Guid Id { get; set; }
