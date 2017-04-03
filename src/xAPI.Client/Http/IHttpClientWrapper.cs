0bbf2325bf91b193c77639ac5eb9b94f605a1d02
Maxime Rossini <mrossini@maskott.com>
2017-04-04 00:42:57 +0200

    remove useless classes

--- src/xAPI.Client/Http/IHttpClientWrapper.cs
+++ src/xAPI.Client/Http/IHttpClientWrapper.cs
@@ -1,5 +1,4 @@
 ﻿using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using xAPI.Client.Http.Options;
 
@@ -11,16 +10,5 @@ namespace xAPI.Client.Http
         Task<HttpResponseMessage> PutJson<T>(RequestOptions options, T content);
         Task<HttpResponseMessage> PostJson<T>(RequestOptions options, T content);
         Task<HttpResponseMessage> Delete(RequestOptions options);
-    }
-
-    internal class HttpResult
-    {
-        public HttpResponseHeaders Headers { get; set; }
-    }
-
-    internal class HttpResult<T> : HttpResult
-    {
-        public HttpContentHeaders ContentHeaders { get; set; }
-        public T Content { get; set; }
     }
 }
